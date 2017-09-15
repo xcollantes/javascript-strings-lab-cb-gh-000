62234b087b399e93c244ea45430f5ce8a8973ab5
Xavier Collantes <xcollantes@zagmail.gonzaga.edu>
2017-09-15 23:00:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var specialGuest = "Neil deGrasse Tyson"
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
-var topic = "Let\'s talk about space.";
+var t = "Let\'s talk about space.";
 
-var conversation = `${topic}`;
+var conversation = `${t}`;
